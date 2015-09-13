e87c05b99a4c70c58076edfc5d303a764236a077
andrii-g <andrii.grygoruk@gmail.com>
2015-09-14 00:30:28 +0300

    Create JUnitSuite.java

---
+++ JUnitSuite.java
@@ -0,0 +1,12 @@
+package y8counter;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	TestY8.class
+})
+
+public class JUnitSuite {
+}
